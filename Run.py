c422bccfdbb334b998cf118635da40df869d9bf1
lucio-lpf <lucio-lpf@github.com>
2019-06-16 00:28:00 -0300

    Muda main

--- Run.py
+++ Run.py
@@ -1,26 +1,52 @@
-from LogisticRegression import LogisticRegression
+from DataHandler import DataHandler
 from NeuralNetwork import *
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import math as mt
+import sys
 import csv
-from Neuron import Neuron
 
 alpha = 0.01
 
 #treina o modelo
 
-wine = LogisticRegression("./datasets/teste.csv")
-wine.normalizeData()
+def main():
 
-camadas = [3, 2, 1]
-entradas = len(wine.data[0]) - 1
-nn = NeuralNetwork(entradas, camadas)
+    network_file = "network_default.txt"
+    args = sys.argv[1:]
+    if len(args) is 3:
+        network_file = args[0]
+        initial_weights_file = args[1]
+        dataset_file = args[2]
 
+    else:
+        print("Selecione seu dataset: \n 1 - Ionosphere \n 2 - Pima \n 3 - Wdbc \n 4 - Wine")
+        escolha = int(input("Escolha: "))
+        if escolha is 1:
+            dataset_file = "./datasets/ionosphere.csv"
+        elif escolha is 2:
+            dataset_file = "./datasets/pima.csv"
+        elif escolha is 3:
+            dataset_file = "./datasets/wdbc.csv"
+        elif escolha is 4:
+            dataset_file = "./datasets/wine.csv"
+        else:
+            print("Escolha invalida")
+            exit()
 
-nn.treina_rede(wine.data[0], wine.results[0])
+    camadas = []
+    fator_regularizacao = None
+    with open(network_file) as network:
+        info_camadas = csv.reader(network, delimiter=",", quoting=csv.QUOTE_NONE)
+        for index, row in enumerate(info_camadas):
+            if index is 0:
+                fator_regularizacao = float(row[0])
+            else:
+                camadas.append(int(row[0]))
 
-#for index in range(len(wine.data)):
-#    nn.treina_rede(wine.data[index], wine.results[index])
+    dataset = DataHandler(dataset_file)
+    dataset.normalizeData()
 
+    entradas = len(dataset.data[0]) - 1
+    nn = NeuralNetwork(entradas, camadas)
+    nn.calcula_saidas(dataset.data[0])
+
+if __name__ == '__main__':
+    main()
